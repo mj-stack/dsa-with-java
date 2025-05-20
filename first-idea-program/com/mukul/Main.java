b352f5e51bfb26be46f3d71d9383a048ff15950a
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-20 18:22:57 +0530

    add user input functionality to Main.java

--- first-idea-program/com/mukul/Main.java
+++ first-idea-program/com/mukul/Main.java
@@ -1,7 +1,12 @@
 package com.mukul;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+         System.out.println("Hello World");
+
+         Scanner input = new Scanner(System.in);
+         System.out.println(input.nextLine());
     }
 }
