8001860aebd6b842edc31a82260f15a012b5ff5f
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-21 08:53:23 +0530

    add Basics class with examples of control flow statements

---
+++ first-idea-program/com/mukul/Basics.java
@@ -0,0 +1,25 @@
+package com.mukul;
+
+public class Basics {
+    public static void main(String[] args) {
+        // if statement
+        //  int a = 10;
+        // if (a == 10) {
+        //      System.out.println("Hello World");
+        //  }
+
+        // while loop
+        // int count = 1;
+        // while(count != 6) {
+        //    System.out.println(count);
+        //    count++;
+        // }
+
+        // for loop
+        for (int count = 1; count != 6 ; count++) {
+            System.out.println(count);
+        }
+    }
+
+
+}
