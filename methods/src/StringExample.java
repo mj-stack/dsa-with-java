5ee24d0b2a11bc284fad7443f532c57dc81f8813
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-23 20:49:42 +0530

    refactor StringExample and Sum classes for improved greeting and addition functionality

--- methods/src/StringExample.java
+++ methods/src/StringExample.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class StringExample {
     public static void main(String[] args) {
-        String message = greet();
-        System.out.println(message);
+        // String message = greet();
+        // System.out.println(message);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println(myGreet(name));
+    }
+
+    static String myGreet(String name) {
+        return "Hello " + name;
     }
 
     static String greet() {
--- methods/src/Sum.java
+++ methods/src/Sum.java
@@ -2,26 +2,37 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        int sum = sum();
+        Scanner input = new Scanner(System.in);
+        int num1, num2;
+        System.out.print("Enter number 1: ");
+        num1 = input.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = input.nextInt();
+        int sum = sum(num1, num2);
         System.out.println("The sum of the operation is: " + sum);
     }
 
-    // return the value
-    static int sum() {
-        int sum = 0;
-
-        Scanner input = new Scanner(System.in);
-        int num1, num2;
-
-        System.out.print("Enter number 1: ");
-        num1 = input.nextInt();
-
-        System.out.print("Enter number 2: ");
-        num2 = input.nextInt();
-        sum = num1 + num2;
-
-        return sum;
+    // pass the value of numbers when you are calling the method in main()
+    static int sum(int num1, int num2) {
+        return num1 + num2;
     }
+
+    // return the value
+//    static int sum() {
+//        int sum = 0;
+//
+//        Scanner input = new Scanner(System.in);
+//        int num1, num2;
+//
+//        System.out.print("Enter number 1: ");
+//        num1 = input.nextInt();
+//
+//        System.out.print("Enter number 2: ");
+//        num2 = input.nextInt();
+//        sum = num1 + num2;
+//
+//        return sum;
+//    }
 
 //    static void sum() {
 //        Scanner input = new Scanner(System.in);
