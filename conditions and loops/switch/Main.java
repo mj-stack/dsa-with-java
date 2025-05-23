b2ceb74132fa16f7983a65c16c08164ac251b430
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-23 18:14:03 +0530

    update Main class to switch between weekdays and weekends based on user input

--- conditions and loops/switch/Main.java
+++ conditions and loops/switch/Main.java
@@ -2,14 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String fruit = input.next();
+//        Scanner input = new Scanner(System.in);
+//        String fruit = input.next();
+//
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of fruits");
+//            case "Apple" -> System.out.println("A sweet red fruit");
+//            case "Orange" -> System.out.println("Rich in vitamin c");
+//            default -> System.out.println("Please enter a valid fruit");
+//        }
 
-        switch (fruit) {
-            case "Mango" -> System.out.println("King of fruits");
-            case "Apple" -> System.out.println("A sweet red fruit");
-            case "Orange" -> System.out.println("Rich in vitamin c");
-            default -> System.out.println("Please enter a valid fruit");
+//        Scanner input = new Scanner(System.in);
+//        int day = input.nextInt();
+//
+//        switch (day) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//            default -> System.out.println("Not a valid day");
+//        }
+
+        Scanner input = new Scanner(System.in);
+        int day = input.nextInt();
+
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 6, 7 -> System.out.println("Weekend");
+            default -> System.out.println("Not a valid day");
         }
     }
 }
