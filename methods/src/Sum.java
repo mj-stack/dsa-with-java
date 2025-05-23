a39c6272c8bac6ded66a6545e8b40a8cf303f7f7
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-23 20:33:03 +0530

    add Greeting and StringExample classes; update Sum class to return sum value

--- methods/src/Sum.java
+++ methods/src/Sum.java
@@ -2,25 +2,43 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+        int sum = sum();
+        System.out.println("The sum of the operation is: " + sum);
     }
 
-    static void sum() {
+    // return the value
+    static int sum() {
+        int sum = 0;
+
         Scanner input = new Scanner(System.in);
-        int num1, num2, sum;
+        int num1, num2;
 
         System.out.print("Enter number 1: ");
         num1 = input.nextInt();
 
         System.out.print("Enter number 2: ");
         num2 = input.nextInt();
-
         sum = num1 + num2;
-        System.out.println("The sum is: " + sum);
+
+        return sum;
     }
 
+//    static void sum() {
+//        Scanner input = new Scanner(System.in);
+//        int num1, num2, sum;
+//
+//        System.out.print("Enter number 1: ");
+//        num1 = input.nextInt();
+//
+//        System.out.print("Enter number 2: ");
+//        num2 = input.nextInt();
+//
+//        sum = num1 + num2;
+//        System.out.println("The sum is: " + sum);
+//    }
+
     /*
-        return_type name() {
+        return_type name(arguments) {
             // body
             return statement;
         }
