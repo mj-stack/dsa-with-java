7d752f23bde9a09c5bcc1ed6a0737620d85134e1
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-24 10:18:49 +0530

    add ChangeValue and PassingExample classes to demonstrate array modification and string passing behavior

---
+++ methods/src/PassingExample.java
@@ -0,0 +1,12 @@
+package src;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Mukul";
+        changeName(name);
+    }
+
+    static void changeName(String name) {
+        name = "Anmol";
+    }
+}
