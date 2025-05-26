2e0de15f03314a78d9c24ffb1eae75c526425814
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-26 12:41:20 +0530

    2d arrays

---
+++ arrays/code/src/MultiDimensionalArray.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        /*
+            1 2 3
+            4 5 6
+            7 8 9
+         */
+
+        // int[][] arr = new int[3][3];
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5},
+                {6, 7, 8, 9},
+        };
+
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+        }
+    }
+}
