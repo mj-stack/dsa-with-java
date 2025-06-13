9ae3aa2f021af6b7f4d3fa31b59c4b765f92e927
Mukul Joshi <mjoshi1819@gmail.com>
2025-06-13 21:44:27 +0530

    add ValidPalindrome class with implementation for checking if a string is a palindrome

---
+++ leetcodeQuestions/ValidPalindrome.java
@@ -0,0 +1,10 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        return false;
+    }
+}
