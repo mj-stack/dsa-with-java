fba16d462b34a5e71b4c7f85b9517d21a0214b36
Mukul Joshi <mjoshi1819@gmail.com>
2025-06-23 11:12:57 +0530

    add PrintFactorial class with implementation for calculating factorial of a number

---
+++ recursion/PrintFactorial.java
@@ -0,0 +1,13 @@
+public class PrintFactorial {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        return n * factorial(n - 1);
+    }
+}
