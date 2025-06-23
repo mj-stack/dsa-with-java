e77e2439e94c3ff4025dbb0455166b5582f7447f
Mukul Joshi <mjoshi1819@gmail.com>
2025-06-23 11:23:14 +0530

    add SumOfNNumbers class with implementation for calculating the sum of the first N natural numbers

---
+++ recursion/SumOfNNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfNNumbers {
+    public static void main(String[] args) {
+        System.out.println(sum(6));
+    }
+
+    public static int sum(int n) {
+        if (n == 1) {
+            return n;
+        }
+
+        return n + sum(n - 1);
+    }
+}
