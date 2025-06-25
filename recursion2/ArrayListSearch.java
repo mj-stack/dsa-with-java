1306959d5a070ba9099374a783da424c47fb9292
Mukul Joshi <mjoshi1819@gmail.com>
2025-06-25 13:28:00 +0530

    add ArrayListSearch class with implementation for linear search using ArrayList

---
+++ recursion2/ArrayListSearch.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ArrayListSearch {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 4, 5, 5, 5, 8};
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> finalList = linearSearch(array, 5, 0, list);
+        System.out.println(finalList);
+    }
+
+    public static ArrayList<Integer> linearSearch(int[] array, int target, int index, ArrayList<Integer> list) {
+        if (index == array.length) {
+            return list;
+        }
+        if (array[index] == target) {
+            list.add(index);
+        }
+        return linearSearch(array, target, ++index, list);
+    }
+}
+
+
