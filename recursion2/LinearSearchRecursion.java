8eaddd85264269ab8c46d180eacf79a38c5b34b2
Mukul Joshi <mjoshi1819@gmail.com>
2025-06-25 12:07:19 +0530

    fix LinearSearchRecursion to handle out-of-bounds index in linear search

--- recursion2/LinearSearchRecursion.java
+++ recursion2/LinearSearchRecursion.java
@@ -1,15 +1,15 @@
 public class LinearSearchRecursion {
     public static void main(String[] args) {
         int[] array = {3, 2, 1, 18, 9};
-        System.out.println(linearSearch(array, 18, 0));
+        System.out.println(linearSearch(array, 9, 0));
     }
 
     public static int linearSearch(int[] array, int target, int index) {
-        if (index == array.length - 1) {
-            return array[index] == target ? index : - 1;
+        if (index >= array.length) {
+            return - 1;
         }
 
-        return array[index] == target ? index : (linearSearch(array, target, ++index));
+        return array[index] == target ? index : (linearSearch(array, target, index + 1));
     }
 
 }
