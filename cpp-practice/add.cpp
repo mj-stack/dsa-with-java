845db24d43aea9288d49cae4d1b3c029ed7356f3
Mukul Joshi <mjoshi1819@gmail.com>
2025-06-29 11:25:57 +0530

    add add.cpp with implementation for basic addition of two numbers

---
+++ cpp-practice/add.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    // Taking input from user
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Calculating sum
+    sum = num1 + num2;
+
+    // Displaying result
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
